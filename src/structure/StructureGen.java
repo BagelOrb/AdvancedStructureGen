e983ca8480150a9020c43cb5ffe061a5a866c139
JBMorion <jorismaster@hotmail.com>
2015-05-06 00:05:22 +0200

    Wall Gen added

--- src/structure/StructureGen.java
+++ src/structure/StructureGen.java
@@ -7,10 +7,16 @@ import org.bukkit.block.BlockFace;
 public class StructureGen {
 
 	public static void generate(Block location) {
-		int w=5, h=5, d=5;
+		int w=6, h=3, d=8;
 		Room room = new Room(location, w, h, d);
 		Placement.liftStructure(room);
 		Floor.setFloorMat(room, Material.WOOD);
+		
+		Wall.setWallMat(room, BlockFace.NORTH, Material.COBBLESTONE);
+		Wall.setWallMat(room, BlockFace.EAST, Material.COBBLESTONE);
+		Wall.setWallMat(room, BlockFace.SOUTH, Material.COBBLESTONE);
+		Wall.setWallMat(room, BlockFace.WEST, Material.COBBLESTONE);
+		Wall.setWallCornersMat(room, Material.LOG);
 		
 		Roof.makeRoof(room, BlockFace.NORTH);
 	}
