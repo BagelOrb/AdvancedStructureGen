0dd0d90257ae036189828cddbd13d4bad85624b8
BagelOrb <t.kuipers@ultimaker.com>
2015-05-05 21:20:47 +0200

    first working try

--- src/main/AdvancedStructureGen.java
+++ src/main/AdvancedStructureGen.java
@@ -1,8 +1,44 @@
 package main;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.plugin.PluginManager;
+import com.massivecraft.massivecore.MassivePlugin;
 
-public class AdvancedStructureGen extends JavaPlugin {
+import commands.CmdASG;
 
 
+
+public class AdvancedStructureGen extends MassivePlugin {
+
+	public CmdASG outerCommand;
+
+	public static World defaultWorld;
+	
+	
+	public static AdvancedStructureGen getCurrentPlugin() {
+//		return currentPlugin; 
+		return (AdvancedStructureGen) Bukkit.getServer().getPluginManager().getPlugin("AdvancedStructureGen"); 
+	}
+	
+	@Override
+	public void onEnable() {
+		
+
+		defaultWorld = Bukkit.getServer().getWorld("world");
+
+
+		PluginManager pm = getServer().getPluginManager();
+//		pm.registerEvents(this.AllListener, this);		
+		outerCommand = new CmdASG();
+		outerCommand.register(getCurrentPlugin());
+		
+//		new BukkitRunnable() {
+//			
+//			@Override
+//			public void run() {
+//			    IO.checkAllFactions();
+//			}
+//		}.runTaskLater(this, 10);
+	}
 }
